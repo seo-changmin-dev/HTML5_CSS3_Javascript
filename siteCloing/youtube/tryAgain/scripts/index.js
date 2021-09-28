f4563245f9a5ecdf91fbd41fd1db1ee9b855fdd1
seo.changmin.dev <seo.changmin.dev@gmail.com>
2021-09-28 21:25:50 +0900

    make toggle on subscribe button

--- siteCloing/youtube/tryAgain/scripts/index.js
+++ siteCloing/youtube/tryAgain/scripts/index.js
@@ -1,3 +1,11 @@
-@media screen and (min-width: 768px) {
-    
-}
+const subscribeBtn = document.querySelector('.channel_info > .subscribe_btn');
+
+subscribeBtn.addEventListener('click', () => {
+  subscribeBtn.classList.toggle('active');
+
+  if(subscribeBtn.innerText === 'SUBSCRIBE') {
+    subscribeBtn.innerText = 'SUBSCRIBED';
+  } else {
+    subscribeBtn.innerText = 'SUBSCRIBE'
+  }
+})
